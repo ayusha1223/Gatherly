96b28dbd73eb08546ef1c78faa529f1aa33c7790
resha-ad <adhikariresha@gmail.com>
2025-02-15 19:18:00 +0545

    Added authentication route and setup express server

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import cors from 'cors';
+import authRoutes from './src/routes/authRoutes.js';
+import sequelize from './src/config/db.js';
+import User from './src/models/User.js';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+sequelize.sync()
+  .then(() => {
+    console.log('Database synced!');
+  })
+  .catch((error) => {
+    console.error('Error syncing database:', error);
+  });
+
+app.listen(PORT, async () => {
+  console.log(`Server running on port ${PORT}`);
+  try {
+    await sequelize.authenticate();
+    console.log('Database connected');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+});
